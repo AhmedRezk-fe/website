6aced55fe86ca6cfc54381114202afa3f9c321d4
unknown <ahmer.rezk.fe@gmail.com>
2020-04-25 05:59:18 +0200

    first commit

--- website/introapp/gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp         = require('gulp'),
-    minyImg      = require('gulp-image');
-
-
-gulp.task('minyImg1', function () {
-    gulp.src('img/**/*').pipe(minyImg()).pipe(gulp.dest('img/min'));
-    });
-
-gulp.task('default', ['minyImg1'])
